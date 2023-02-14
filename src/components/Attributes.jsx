4be8684f36d1d72486f1a175ff7afce284f815ed
Harsh <harsh.anand20999@gmail.com>
2023-02-15 00:06:08 +0530

    Second Sub-form Added

--- src/components/Attributes.jsx
+++ src/components/Attributes.jsx
@@ -3,12 +3,60 @@ const formOneAttributes = {
   age: "",
   area: "",
   location: "",
+  education: "",
+  educationLevel: "",
   smoking: "",
   alcohol: "",
   weight: "",
   height: "",
+  caesarean: "",
+  complication: "",
+  gravida: "",
+  parity: "",
+  mensuralDate: "",
+  expectedDeliveryDate: "",
+  gestationalAge: "",
+  systolicBP: "",
+  diastolicBP: "",
+  haemoglobin: "",
+  fetalHeartRate: "",
+  highComplication: "",
 };
 
 const areaDropDown = ["Rural Area", "Urban Area"];
 
-export { formOneAttributes, areaDropDown };
+const educationDropDown = ["Literate", "Illiterate"];
+
+const gravidaDropDown = [
+  "G1",
+  "G2",
+  "G3",
+  "G4",
+  "G5",
+  "G6",
+  "G7",
+  "G8",
+  "G9",
+  "G10",
+];
+
+const parityDropDown = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+const heartRateDropDown = [
+  "100-109",
+  "110-119",
+  "120-129",
+  "130-139",
+  "140-149",
+  "150-159",
+  "160-170",
+];
+
+export {
+  formOneAttributes,
+  areaDropDown,
+  educationDropDown,
+  gravidaDropDown,
+  parityDropDown,
+  heartRateDropDown,
+};
--- src/components/FormOne.jsx
+++ src/components/FormOne.jsx
@@ -3,10 +3,19 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import Theme from "./Theme";
-import { formOneAttributes, areaDropDown } from "./Attributes";
+import {
+  formOneAttributes,
+  areaDropDown,
+  educationDropDown,
+  gravidaDropDown,
+  parityDropDown,
+  heartRateDropDown,
+} from "./Attributes";
 
 function FormOne() {
   const [text, setText] = useState(formOneAttributes);
+
+  const [expand, setExpand] = useState(false);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -28,12 +37,14 @@ function FormOne() {
   }
 
   return (
-    <div>
+    <div className="form-one">
+      {/* SUB FORM 1 */}
       <form className="sub-form">
         <h4 className="sub-heading">
           Basic details of Patient / मरीज़ का मूल विवरण
         </h4>
 
+        {/* NAME */}
         <div className="input-div">
           <label>Name: </label>
           <input
@@ -47,6 +58,7 @@ function FormOne() {
           />
         </div>
 
+        {/* AGE */}
         <div className="input-div">
           <label>Age: </label>
           <input
@@ -62,6 +74,7 @@ function FormOne() {
           />
         </div>
 
+        {/* AREA */}
         <div className="drop-down">
           <label className="drop-down-label">Area:</label>
           <Autocomplete
@@ -75,13 +88,14 @@ function FormOne() {
               setText((prevText) => {
                 return {
                   ...prevText,
-                  area: value
+                  area: value,
                 };
               });
             }}
           />
         </div>
 
+        {/* LOCATION */}
         <div className="input-div">
           <label>Location: </label>
           <input
@@ -95,6 +109,50 @@ function FormOne() {
           />
         </div>
 
+        {/* EDUCATION */}
+        <div className="drop-down">
+          <label className="drop-down-label">Education:</label>
+          <Autocomplete
+            disablePortal
+            className="drop-box-input"
+            id="combo-box2"
+            options={educationDropDown}
+            sx={{ width: 4 / 5 }}
+            renderInput={(params) => <TextField {...params} />}
+            onChange={(event, value) => {
+              // Conditional Rendering
+              if (value === "Literate") {
+                setExpand(true);
+              } else {
+                setExpand(false);
+              }
+
+              setText((prevText) => {
+                return {
+                  ...prevText,
+                  education: value,
+                };
+              });
+            }}
+          />
+        </div>
+
+        {/* LEVEL OF EDUCATION (with Conditional Rendering)*/}
+        {expand && (
+          <div className="input-div">
+            <label>Level of Education: </label>
+            <input
+              name="educationLevel"
+              type="text"
+              onChange={handleChange}
+              value={text.educationLevel}
+              placeholder="Enter Education Level."
+              autoComplete="off"
+            />
+          </div>
+        )}
+
+        {/* SMOKING HABIT */}
         <div className="input-div">
           <label>Smoking Habit:</label>
           <div className="radio-div">
@@ -119,6 +177,7 @@ function FormOne() {
           </div>
         </div>
 
+        {/* ALCOHOL INTAKE */}
         <div className="input-div">
           <label>Alcohol Intake:</label>
           <div className="radio-div">
@@ -143,6 +202,7 @@ function FormOne() {
           </div>
         </div>
 
+        {/* WEIGHT */}
         <div className="input-div">
           <label>Weight: </label>
           <input
@@ -158,6 +218,7 @@ function FormOne() {
           />
         </div>
 
+        {/* HEIGHT */}
         <div className="input-div">
           <label>Height: </label>
           <input
@@ -170,6 +231,209 @@ function FormOne() {
             placeholder="Enter Height (in cms)."
             autoComplete="off"
             required
+          />
+        </div>
+      </form>
+
+      {/* SUB FORM 2 */}
+      <form className="sub-form">
+        <h4 className="sub-heading">Related Information</h4>
+
+        {/* PREVIOUS CAESAREAN */}
+        <div className="input-div">
+          <label>Previous Caesarean:</label>
+          <div className="radio-div">
+            <input
+              name="caesarean"
+              type="radio"
+              onChange={handleChange}
+              checked={text.caesarean === "yes"}
+              value="yes"
+              className="radio-button"
+            />
+            <label className="radio-label">Yes</label>
+            <input
+              name="caesarean"
+              type="radio"
+              onChange={handleChange}
+              checked={text.caesarean === "no"}
+              value="no"
+              className="radio-button"
+            />
+            <label className="radio-label">No</label>
+          </div>
+        </div>
+
+        {/* PREVIOUS COMPLICATION */}
+        <div className="input-div">
+          <label>Previous Complication: </label>
+          <input
+            name="complication"
+            type="text"
+            onChange={handleChange}
+            value={text.complication}
+            placeholder="Enter any Previous Complication."
+            autoComplete="off"
+            required
+          />
+        </div>
+
+        {/* GRAVIDA */}
+        <div className="drop-down">
+          <label className="drop-down-label">Gravida:</label>
+          <Autocomplete
+            disablePortal
+            className="drop-box-input"
+            id="combo-box"
+            options={gravidaDropDown}
+            sx={{ width: 4 / 5 }}
+            renderInput={(params) => <TextField {...params} />}
+            onChange={(event, value) => {
+              setText((prevText) => {
+                return {
+                  ...prevText,
+                  gravida: value,
+                };
+              });
+            }}
+          />
+        </div>
+
+        {/* PARITY */}
+        <div className="drop-down">
+          <label className="drop-down-label">Parity:</label>
+          <Autocomplete
+            disablePortal
+            className="drop-box-input"
+            id="combo-box"
+            options={parityDropDown}
+            sx={{ width: 4 / 5 }}
+            renderInput={(params) => <TextField {...params} />}
+            onChange={(event, value) => {
+              setText((prevText) => {
+                return {
+                  ...prevText,
+                  parity: value,
+                };
+              });
+            }}
+          />
+        </div>
+
+        {/* LAST MENSURAL DATE */}
+        <div className="input-div">
+          <label>Last Mensural Date: </label>
+          <input
+            name="mensuralDate"
+            type="date"
+            onChange={handleChange}
+            value={text.mensuralDate}
+            // placeholder=""
+            autoComplete="off"
+            required
+          />
+        </div>
+
+        {/* EXPECTED DELIVERY DATE */}
+        <div className="input-div">
+          <label>Expected Delivery Date: </label>
+          <input
+            name="expectedDeliveryDate"
+            type="date"
+            onChange={handleChange}
+            value={text.expectedDeliveryDate}
+            // placeholder=""
+            autoComplete="off"
+            required
+          />
+        </div>
+
+        {/* GESTATIONAL AGE */}
+        <div className="input-div">
+          <label>Gestational Age: </label>
+          <input
+            name="gestationalAge"
+            type="text"
+            onChange={handleChange}
+            value={text.gestationalAge}
+            placeholder="Enter Gestational Age."
+            autoComplete="off"
+            required
+          />
+        </div>
+
+        {/* BLOOD PRESSURE */}
+        <div className="input-div">
+          <label>Blood Pressure: </label>
+          <br />
+          <label>Systolic: </label>
+          <input
+            name="systolicBP"
+            type="text"
+            onChange={handleChange}
+            value={text.systolicBP}
+            placeholder="Enter Systolic BP."
+            autoComplete="off"
+            required
+          />
+          <br />
+          <label>Diastolic: </label>
+          <input
+            name="diastolicBP"
+            type="text"
+            onChange={handleChange}
+            value={text.diastolicBP}
+            placeholder="Enter Diastolic BP."
+            autoComplete="off"
+            required
+          />
+        </div>
+
+        {/* HAEMOGLOBIN */}
+        <div className="input-div">
+          <label>Haemoglobin: </label>
+          <input
+            name="haemoglobin"
+            type="text"
+            onChange={handleChange}
+            value={text.haemoglobin}
+            placeholder="Enter Haemoglobin Level."
+            autoComplete="off"
+            required
+          />
+        </div>
+
+        {/* FETAL HEART RATE */}
+        <div className="drop-down">
+          <label className="drop-down-label">Fetal Heart Rate:</label>
+          <Autocomplete
+            disablePortal
+            className="drop-box-input"
+            id="combo-box"
+            options={heartRateDropDown}
+            sx={{ width: 4 / 5 }}
+            renderInput={(params) => <TextField {...params} />}
+            onChange={(event, value) => {
+              setText((prevText) => {
+                return {
+                  ...prevText,
+                  fetalHeartRate: value,
+                };
+              });
+            }}
+          />
+        </div>
+
+        {/* HIGH COMPPLICATION */}
+        <div className="input-div">
+          <label>High Complication in Current Pregnancy: </label>
+          <input
+            name="highComplication"
+            type="text"
+            onChange={handleChange}
+            value={text.highComplication}
+            placeholder="Enter High Complications if any."
+            autoComplete="off"
           />
         </div>
       </form>
